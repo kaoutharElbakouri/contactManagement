ec34f38c03a53c79058cb5a2078f0879e73cd013
kaouthar <73237317+kaoutharElbakouri@users.noreply.github.com>
2021-08-14 14:12:02 +0200

    Create configuration.php

---
+++ configuration.php
@@ -0,0 +1,10 @@
+<?php
+//Chemin vers le rÃ©pÃ©rtoire qui va contenir les fichiers envoyÃ©s aux serveurs
+$UPLOAD_DIR = __DIR__ . '/../../upload/';
+
+//Chemin vers le rÃ©pÃ©rtoire de l'application
+$APP_DIR = __DIR__ . '\\..\\';
+
+//URL vers le rÃ©pÃ©rtoire parent de l'application
+$APP_URL ="http://localhost/DL//";
+$APP_UPLOAD_URL ="http://localhost/DL//upload/";
